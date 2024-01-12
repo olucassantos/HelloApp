74d24d3f509d1128354acf33f4a85b01900dc94b
Lucas Anjos <oargus.g@gmail.com>
2024-01-11 21:46:11 -0300

    Primeiro commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// Imprime um texto na tela.
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("Seja bem-vindo ao meu primeiro programa!");
+
+// Criação de duas variáveis.
+string nome;
+int idade;
+
+Console.WriteLine("Qual é o seu nome?");
+
+// Lê o nome digitado pelo usuário e armazena na variável nome.
+nome = Console.ReadLine();
+Console.WriteLine("Prazer em conhecer" + nome + "!");
+
+Console.WriteLine("Qual é a sua idade?");
+idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você tem " + idade + " anos de idade.");
